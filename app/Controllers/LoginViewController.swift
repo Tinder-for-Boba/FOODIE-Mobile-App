bbd66f5c72d7f63a5313de705a423420747deddf
Paul Pan <panjunhong05@gmail.com>
2020-04-11 13:14:44 -0700

    Intial code commit

---
+++ app/Controllers/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  app
+//
+//  Created by Paul Pan on 20/4/11.
+//  Copyright © 2020 Foodie Inc. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
