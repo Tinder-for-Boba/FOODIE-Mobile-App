774ee6f7e1c27e27a2a9c2aeebe35c07928fef7c
soyoybean <sok020@ucsd.edu>
2020-04-23 11:30:53 -0700

    Create all pages

---
+++ app/Controllers/AddStoryViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddStoryViewController.swift
+//  app
+//
+//  Created by Soyon Kim on 4/23/20.
+//  Copyright © 2020 Foodie Inc. All rights reserved.
+//
+
+import UIKit
+
+class AddStoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ app/Controllers/RestaurantDetailsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RestaurantDetailsViewController.swift
+//  app
+//
+//  Created by Soyon Kim on 4/23/20.
+//  Copyright © 2020 Foodie Inc. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
